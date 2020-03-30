ee75c853f30bbc8c987f3ea0ac3a7f1b7544b558
allenroberts1 <50081814+allenroberts1@users.noreply.github.com>
2020-03-30 13:21:07 -0400

    First

---
+++ ReactStuff/hackernews/build/precache-manifest.a9eb2655d3af730e629e0df573ed13fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e4ce3e32e4e01e605e9813e6d7aa5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b8a658b11f9b809761",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a1f8cbda18c9cf42d62",
+    "url": "/static/js/2.ff4b4a9a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ff4b4a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b8a658b11f9b809761",
+    "url": "/static/js/main.a73dbe6c.chunk.js"
+  },
+  {
+    "revision": "9698ff32d4ea6fc65ae9",
+    "url": "/static/js/runtime-main.8b6ae471.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
